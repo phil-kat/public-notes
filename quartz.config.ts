8c23819b61a3776b3f180881803da39fba3994d4
phil-kat <140596832+phil-kat@users.noreply.github.com>
2025-09-10 01:55:41 +0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "ru-RU",
+    locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",

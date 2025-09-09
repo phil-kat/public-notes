96ec03027d79e3efc7b91135e1414740d63d3d46
phil-kat <140596832+phil-kat@users.noreply.github.com>
2025-09-09 23:18:26 +0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Дом, в котором",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
